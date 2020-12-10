e7dadcaeca32f05ee3ecb33236b6193e4e283ad0
hc07180011 <henrychao0718@gmail.com>
2020-12-11 01:02:00 +0800

    angle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 import numpy as np
 from pybaseball_handler import find_player_by_id, get_history_data
-from plot_statistics import plot_field, plot_distance
+from plot_statistics import plot_field, plot_distance, plot_angle
 
 
 def clear_nan(arr):
@@ -10,5 +11,6 @@ def clear_nan(arr):
 if __name__ == '__main__':
     pybaseball_data = get_history_data('2000-01-01', '2017-01-01', batter=find_player_by_id('David Ortiz'))
     X, Y = clear_nan(pybaseball_data['hc_x']), clear_nan(pybaseball_data['hc_y'])
-    plot_field(X, Y, 'drops.png')
-    plot_distance(X, Y, 'distance.png')
+    plot_field(X, Y, os.path.join('img', 'slides', 'drops.png'))
+    plot_distance(X, Y, os.path.join('img', 'slides', 'distance.png'))
+    plot_angle(X, Y, os.path.join('img', 'slides', 'angle.png'))
--- plot_statistics.py
+++ plot_statistics.py
@@ -1,7 +1,21 @@
+import os
+import imageio
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import UnivariateSpline
 from tuning import fileding_range
+
+
+def gen_gif(img_dir, gif_path):
+    images = []
+    filenames = os.listdir(img_dir)
+    filenames = sorted(filenames)
+    for filename in filenames:
+        try:
+            images.append(imageio.imread(os.path.join(img_dir, filename)))
+        except:
+            pass
+    imageio.mimsave(gif_path, images, duration=(12.0 / len(images)))
 
 
 def plot_field(X, Y, fig_path, annotations=[]):
@@ -12,12 +26,33 @@ def plot_field(X, Y, fig_path, annotations=[]):
     plt.savefig(fig_path)
     plt.close()
 
+
+def pdf_to_bin(p, bin):
+    p, x = np.histogram(p, bins=bin)
+    zeros = np.where(np.array(p) == 0)[0]
+    for zero in zeros:
+        if zero and zero != len(p)-1:
+            p[zero] = (p[zero-1] + p[zero+1]) / 2.0
+    return x[1:], p / np.sum(p)
+
+
 def plot_distance(X, Y, fig_path):
     drops = np.transpose(np.array([X, Y]), (1, 0))
     dis = np.sqrt(drops * drops).astype('int')
-    p, x = np.histogram(dis, bins=1000)
-    plt.plot(x[1:], p / np.sum(p))
+    x, p = pdf_to_bin(dis, 200)
+    plt.plot(x, p)
     plt.xlabel('distance')
     plt.ylabel('ratio')
     plt.savefig(fig_path)
+    plt.close()
+
+
+def plot_angle(X, Y, fig_path):
+    angle = np.arctan2(X, Y) * 180.0 / np.pi
+    x, p = pdf_to_bin(angle, 360)
+    plt.xticks(np.arange(-180, 181, 30))
+    plt.plot(x, p)
+    plt.xlabel('degree')
+    plt.ylabel('ratio')
+    plt.savefig('angle.png')
     plt.close()
